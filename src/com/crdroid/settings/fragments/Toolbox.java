8237578b9904fbcb8dc978cd17c850c53744955b
Alvin Francis <nivlafx@gmail.com>
2024-06-21 04:15:16 -0400

    Toolbox: Add option to spoof google apps
     * For non pixel devices
    
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- src/com/crdroid/settings/fragments/Toolbox.java
+++ src/com/crdroid/settings/fragments/Toolbox.java
@@ -50,6 +50,7 @@ public class Toolbox extends SettingsPreferenceFragment implements Preference.On
     public static final String TAG = "Toolbox";
     private static final String KEY_QUICKSWITCH_PREFERENCE = "quickswitch";
     private static final String SYS_GMS_SPOOF = "persist.sys.pixelprops.gms";
+    private static final String SYS_GOOGLE_SPOOF = "persist.sys.pixelprops.google";
     private static final String SYS_PROP_OPTIONS = "persist.sys.pixelprops.all";
     private static final String SYS_NETFLIX_SPOOF = "persist.sys.pixelprops.netflix";
     private static final String SYS_GPHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
@@ -57,6 +58,7 @@ public class Toolbox extends SettingsPreferenceFragment implements Preference.On
     private boolean isPixelDevice;
 
     private Preference mGmsSpoof;
+    private Preference mGoogleSpoof;
     private Preference mGphotosSpoof;
     private Preference mNetflixSpoof;
     private Preference mPropOptions;
@@ -77,6 +79,7 @@ public class Toolbox extends SettingsPreferenceFragment implements Preference.On
         mNetflixSpoof = (Preference) findPreference(SYS_NETFLIX_SPOOF);
         mGphotosSpoof = (Preference) findPreference(SYS_GPHOTOS_SPOOF);
         mGmsSpoof = (Preference) findPreference(SYS_GMS_SPOOF);
+        mGoogleSpoof = (Preference) findPreference(SYS_GOOGLE_SPOOF);
         mPropOptions = (Preference) findPreference(SYS_PROP_OPTIONS);
         isPixelDevice = SystemProperties.get("ro.soc.manufacturer").equals("Google");
         if (!isPixelDevice) {
@@ -86,15 +89,19 @@ public class Toolbox extends SettingsPreferenceFragment implements Preference.On
             mGmsSpoof.setDependency(SYS_PROP_OPTIONS);
             mGphotosSpoof.setDependency(SYS_PROP_OPTIONS);
             mNetflixSpoof.setDependency(SYS_PROP_OPTIONS);
+            mGoogleSpoof.setEnabled(false);
+            mGoogleSpoof.setSummary(R.string.google_spoof_option_disabled);
         }
         mGmsSpoof.setOnPreferenceChangeListener(this);
         mPropOptions.setOnPreferenceChangeListener(this);
+        mGoogleSpoof.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mGmsSpoof 
-            || preference == mPropOptions) {
+            || preference == mPropOptions
+            || preference == mGoogleSpoof) {
             SystemRestartUtils.showSystemRestartDialog(getContext());
             return true;
         }
