0a462310879e4ec29ae1ebd99dab865795199d97
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-26 15:05:13 +0800

    Implement wallpaper styles
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/rising/settings/fragments/Themes.java
+++ src/com/rising/settings/fragments/Themes.java
@@ -28,35 +28,20 @@ import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
-import com.android.settings.utils.SystemRestartUtils;
-
 @SearchIndexable
-public class Themes extends SettingsPreferenceFragment 
-            implements Preference.OnPreferenceChangeListener {
+public class Themes extends SettingsPreferenceFragment {
 
     public static final String TAG = "Themes";
-    
-    private Preference mBlurWpPref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.rising_settings_themes);
-        mBlurWpPref = findPreference("persist.sys.wallpaper.blur_enabled");
-        mBlurWpPref.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.VIEW_UNKNOWN;
-    }
-    
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mBlurWpPref) {
-          SystemRestartUtils.showSystemUIRestartDialog(getContext());
-          return true;
-        }
-        return false;
     }
 
     /**
