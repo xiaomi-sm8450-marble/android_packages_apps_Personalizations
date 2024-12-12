db25b1671dac12ce1e9ad66cca3bed80fd4d86d6
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-12 16:55:23 +0800

    Add notification and progress bar theme styles
    Co-authored-by: DrDisagree <crazymahmud08@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/rising/settings/fragments/Themes.java
+++ src/com/rising/settings/fragments/Themes.java
@@ -17,6 +17,8 @@ package com.rising.settings.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 import androidx.preference.Preference;
 
@@ -26,17 +28,114 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.android.internal.util.android.ThemeUtils;
+
 import java.util.List;
 
 @SearchIndexable
-public class Themes extends SettingsPreferenceFragment {
+public class Themes extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "Themes";
+    private static final String KEY_PGB_STYLE = "progress_bar_style";
+    private static final String KEY_NOTIF_STYLE = "notification_style";
+    
+    private ThemeUtils mThemeUtils;
+    private Preference mProgressBarPref;
+    private Preference mNotificationStylePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.rising_settings_themes);
+        mThemeUtils = ThemeUtils.getInstance(getActivity());
+        mProgressBarPref = (Preference) findPreference(KEY_PGB_STYLE);
+        mProgressBarPref.setOnPreferenceChangeListener(this);
+        mNotificationStylePref = (Preference) findPreference(KEY_NOTIF_STYLE);
+        mNotificationStylePref.setOnPreferenceChangeListener(this);
+    }
+    
+    private void updateNotifStyle() {
+        final int notifStyle = Settings.System.getIntForUser(
+                getContext().getContentResolver(),
+                KEY_NOTIF_STYLE, 
+                0, 
+                UserHandle.USER_CURRENT
+        );
+        String notifStyleCategory = "android.theme.customization.notification";
+        String overlayThemeTarget = "com.android.systemui";
+        String overlayPackage = null;
+        if (mThemeUtils == null) {
+            mThemeUtils = ThemeUtils.getInstance(getContext());
+        }
+        mThemeUtils.setOverlayEnabled(notifStyleCategory, overlayThemeTarget, overlayThemeTarget);
+        if (notifStyle == 0) return;
+        switch (notifStyle) {
+            case 1:
+                overlayPackage = "com.android.theme.notification.cyberpunk";
+                break;
+            case 2:
+                overlayPackage = "com.android.theme.notification.duoline";
+                break;
+            case 3:
+                overlayPackage = "com.android.theme.notification.ios";
+                break;
+            case 4:
+                overlayPackage = "com.android.theme.notification.layers";
+                break;
+        }
+        if (overlayPackage != null) {
+            mThemeUtils.setOverlayEnabled(notifStyleCategory, overlayPackage, overlayThemeTarget);
+        }
+    }
+
+    private void updateProgressBarStyle() {
+        final int progressBarStyle = Settings.System.getIntForUser(
+                getContext().getContentResolver(),
+                KEY_PGB_STYLE, 
+                0, 
+                UserHandle.USER_CURRENT
+        );
+        String pgbStyleCategory = "android.theme.customization.progress_bar";
+        String overlayThemeTarget = "android";
+        String overlayPackage = null;
+        if (mThemeUtils == null) {
+            mThemeUtils = ThemeUtils.getInstance(getContext());
+        }
+        mThemeUtils.setOverlayEnabled(pgbStyleCategory, overlayThemeTarget, overlayThemeTarget);
+        if (progressBarStyle == 0) return;
+        switch (progressBarStyle) {
+            case 1:
+                overlayPackage = "com.android.theme.progressbar.blocky_thumb";
+                break;
+            case 2:
+                overlayPackage = "com.android.theme.progressbar.minimal_thumb";
+                break;
+            case 3:
+                overlayPackage = "com.android.theme.progressbar.outline_thumb";
+                break;
+        }
+        if (overlayPackage != null) {
+            mThemeUtils.setOverlayEnabled(pgbStyleCategory, overlayPackage, overlayThemeTarget);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mProgressBarPref) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    KEY_PGB_STYLE, value, UserHandle.USER_CURRENT);
+            updateProgressBarStyle();
+            return true;
+        } else if (preference == mNotificationStylePref) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    KEY_NOTIF_STYLE, value, UserHandle.USER_CURRENT);
+            updateNotifStyle();
+            return true;
+        }
+        return false;
     }
 
     @Override
